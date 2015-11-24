9c0b0da3787ec30df65387955286381e9ea91c1b
Anton Rufino <anton.vongola@gmail.com>
2015-11-24 11:46:58 +0800

    Add trim function to util.h

--- util.h
+++ util.h
@@ -8,8 +8,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void cls() {system("clear");}
+
 void header() {
 	printf(" ____            _                   ____  _ \n");
 	printf("/ ___| _   _ ___| |_ ___ _ __ ___   |___ \\/ |\n");
@@ -18,6 +20,11 @@ void header() {
 	printf("|____/ \\__, |___/\\__\\___|_| |_| |_| |_____|_|\n");
 	printf("       |___/                                 \n");
 	printf("\n");
+}
+
+void trim(char str[]) {
+	int i = strlen(str);
+	if (str[i-1] == '\n') str[i-1] = '\0';
 }
 
 #endif
