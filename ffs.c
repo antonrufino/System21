af39646f114ebe9c1ed57d145a9c0d8edda4bb15
Anton Rufino <anton.vongola@gmail.com>
2015-11-25 09:18:41 +0800

    Add TODOs to ffs.c

--- ffs.c
+++ ffs.c
@@ -1,8 +1,15 @@
 #include "menus/main-menu.h"
 
 int main() {
+	// TODO: Initialize linked lists with dummy nodes.
+	// TODO: Load data from file.
+
 	mainMenu();
 	cls();
+
+	// TODO: Save data to file.
+	// TODO: Delete nodes in linked list.
+	// TODO: Delete dummy nodes.
 
 	return 0;
 }
